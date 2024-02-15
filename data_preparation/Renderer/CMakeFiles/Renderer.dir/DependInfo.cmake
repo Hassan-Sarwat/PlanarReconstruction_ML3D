67bee03db29fc60e16237831f223088bdf53cdd3
soniacastrop <sonia.castro@estudiantat.upc.edu>
2024-02-15 20:42:02 +0100

    data_prep

---
+++ data_preparation/Renderer/CMakeFiles/Renderer.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/steve/Documents/ml3d/Project/PlaneNet/data_preparation/Renderer/main.cpp" "/home/steve/Documents/ml3d/Project/PlaneNet/data_preparation/Renderer/CMakeFiles/Renderer.dir/main.cpp.o"
+  "/home/steve/Documents/ml3d/Project/PlaneNet/data_preparation/Renderer/tinyply.cpp" "/home/steve/Documents/ml3d/Project/PlaneNet/data_preparation/Renderer/CMakeFiles/Renderer.dir/tinyply.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/GL"
+  "/usr/local/include/OpenMesh"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
